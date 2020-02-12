b21f1468ac6d8a2336055212380b5b776e7ae968
yokarak <ande67f3@edu.easj.dk>
2020-02-12 21:40:19 +0100

    started with the methods almost finished with team A's

--- app/src/main/java/com/example/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.courtcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int scoreA=0;
+        public int getScoreA() { return scoreA; }
+        public void setScoreA(int scoreA) { this.scoreA = scoreA; }
+    private int scoreB=0;
+        public int getScoreB() { return scoreB; }
+        public void setScoreB(int scoreB) { this.scoreB = scoreB; }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
 
+    public void pointThreeA(View view){ setScoreA(getScoreA()+3); displayForTeamA(scoreA);}
+    public void pointTwoA(View view) { setScoreA(getScoreA()+2); displayForTeamA(scoreA);}
+    public void freeThrowA(View view) { setScoreA(getScoreA()+1); displayForTeamA(scoreA);}
 }
